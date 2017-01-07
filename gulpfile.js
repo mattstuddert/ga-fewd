284a93b96b6701cb70b2b7b622905e75182954e8
mattstuddert <mstuddert@nuveer.com>
2017-01-07 17:13:53 +0000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var livereload = require('gulp-livereload');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('css', function () {
+  gulp.src('src/scss/main.scss')
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(autoprefixer())
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(livereload({ start: true }));
+});
+
+gulp.task('watch:css', ['css'], function() {
+  gulp.watch('src/scss/main.scss', ['css']);
+});
+
+gulp.task('dev', ['watch:css']);
